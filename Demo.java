cdef4257d426792df9508f6cd3f03aa9125a8aa8
sunhai <sh01925846@alibaba-inc.com>
2025-10-22 09:50:31 +0800

    567

---
+++ Demo.java
@@ -0,0 +1,19 @@
+/**
+ * Tom cat
+ * Demo类用于演示基础的Java程序结构和输出功能
+ * 
+ * @author onecar
+ * @version 1.0
+ * @since 2025-10-21
+ */
+public class Demo {
+    /**
+     * 程序入口点
+     * 
+     * @param args 命令行参数
+     */
+    public static void main(String[] args) {
+        // 输出欢迎信息到控制台
+        System.out.println("Helloworld");
+    }
+}
---
+++ hello.py
@@ -0,0 +1,18 @@
+<<<<<<< Local
+<<<<<<< Local
+=======
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+>>>>>>> Remote
+=======
+>>>>>>> Remote
+
+"""
+Python Hello World 程序
+"""
+
+def main():
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    main()
